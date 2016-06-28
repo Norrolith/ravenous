dbcf98b76396ac6145c49b2c1e27ad6b8b44a644
Norrolith <unwary@earthlink.net>
2016-06-27 20:49:40 -0500

    Added find_area function, stopped slime pool hangups

--- assets/scenes/refresh_level.gd
+++ assets/scenes/refresh_level.gd
@@ -6,13 +6,12 @@ var map
 # var b="textvar"
 
 func _ready():
-	map = get_node("/root/main/world_map")
 	set_process(true)
 	
 	pass
 	
 func _process(delta):
 	if(Input.is_action_pressed("ui_accept")):
-		map.my_build_level()
+		tile_info.map.my_build_level()
 
 
